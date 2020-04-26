40d25e392be74e73ba05da08d8c9429544b8c1ec
Irina Fedorova <darmairina@gmail.com>
2020-04-26 13:02:11 +0400

    - User game registration - Game result output - Changes at procedure for adding words - Changes at procedure for checking words

--- hat_bot/hat_bot.py
+++ hat_bot/hat_bot.py
@@ -17,12 +17,58 @@ class HatBot:
         """Constructor"""
         self.words = []
         self.used_words = []
-        self.teams={}
-        self.teams_cnt = 0
+        self.teams = {}
+        self.team_members = {}
+        self.members = []
+        self.member_cnt = 0
+        self.game_number = 0
+        
+    def start_new_game(self):
+        """Start new game, reset lists"""
+        self.words = []
+        self.used_words = []
+        self.game_number = random.randint(1,1000)
+        
+    def reg_member(self, user_name):
+        if user_name in self.members:
+            return 'Участник уже добавлен в игру!'
+        else:
+            self.members.append(user_name)
+            return 'Участник добавлен в игру!'
+        
+    def start_new_round(self):
+        """Start new rownd - exchange values from list of used words to word list"""
+        #если список команд не сформирован, то сформировать
+        #и это первый раунд, оправить каждому участнику номер его команды
+        #если это не первый раунд, то перенести список слов из использованных в общий
+        if len(self.used_words) > 0:
+            self.words = self.used_words
+            self.used_words = []
+        if len(self.members) == 0:
+            return 'Нет зарегистрированных участников игры.'
+        elif len(self.team_members) == 0:
+            return(self.get_teams())
+            
+    def get_teams(self):
+        #self.member_cnt
+        #посчитаем количество зарегистрированных участников
+        random.shuffle(self.members)
+        cnt = len(self.members)
+        for i in range(0, cnt, self.member_cnt):
+            self.team_members[i+1] = self.members[i:i + self.member_cnt]
+        s = ''
+        for key, value in self.team_members.items():
+            s += 'Команда ' + str(key) + ': ' + str(value) + '\n'
+            self.teams[key] = 0
+        return s        
         
     def add_word(self, word):
         """Adding a new word into the word list"""
-        self.words.append(word)
+        if word in self.words:
+            return 'Такое слово уже есть в шляпе'
+        else:
+            self.words.append(word)
+            return 'Слово успешно добавлено'
     
     def get_word(self):
         """Getting a word from word list and put it into list of used words"""
@@ -33,26 +79,27 @@ class HatBot:
             return w
         
     def check_word(self, word, is_ok, team_number = 0):
-        print(team_number)
         if is_ok:
             self.teams[team_number] += 1
             self.used_words.append(word)
         else:
             self.words.append(word)
             
+    def get_result(self):
+        s = ''
+        for key, value in self.teams.items():
+            s += 'команда ' + str(key) + ' - ' + str(value) + ' очков' + '\n'  
+        return s
     
-    def start_new_round(self):
-        """Start new rownd - exchange values from list of used words to word list"""
-        self.words = self.used_words
-        self.used_words = []
-    
-    def start_new_game(self):
-        """Start new game, reset lists"""
-        self.words = []
-        self.used_words = []
-        self.teams = {i+1: 0 for i in range(self.teams_cnt)}
-        print(self.teams_cnt)
-        print(self.teams)
+    def get_help(self):
+        return ("Доступные команды:\n"
+                "/start_game - начать новую игру. Бот пришлет вам номер начатой игры\n"
+                "/reg game_number - заявить об участии в игре, где game_number - номер, полученный при выполнении команды start_game\n"
+                "/round - начать разыгрывать слова из ""шляпы""\n"
+                "/result - подвести итоги игры\n"
+                "любое слово не из списка команд, будет добавлено в ""шляпу""\n"
+                "отправить число = номеру команды - вытянуть слово из ""шляпы"""
+                )
         
     
 def main():
@@ -61,17 +108,23 @@ def main():
     bot = telebot.TeleBot('1099016441:AAHM0pvg9Oa6RyZ-P20gF98XljhgTzVEJMY')       
     
     hat_game = HatBot()
-        
+            
     
     @bot.message_handler(content_types=['text'])
     def get_text_message(message):
-        if message.text == "/start": 
-            bot.send_message(message.from_user.id, 'Поехали! Сколько будет команд?')
-            bot.register_next_step_handler(message, get_teams_cnt);
+        if message.text == "/start_game": 
+            bot.send_message(message.from_user.id, 'Поехали! Сколько человек будет в каждой команде?')
+            bot.register_next_step_handler(message, get_members_cnt);
+        elif message.text == '/help':
+            bot.send_message(message.from_user.id, hat_game.get_help())
+        elif '/reg' in message.text:
+            _, i = message.text.split(' ')
+            bot.send_message(message.from_user.id, hat_game.reg_member(message.from_user.first_name))
         elif message.text == '/round':
-            hat_game.start_new_round()
-            bot.send_message(message.from_user.id, 'Новый раунд')
-        elif message.text.isdigit():# == '1':
+            bot.send_message(message.from_user.id, hat_game.start_new_round())
+        elif message.text == '/result':
+            bot.send_message(message.from_user.id, hat_game.get_result())
+        elif message.text.isdigit():
             team_number = int(message.text)
             word = hat_game.get_word() 
             if word is None:
@@ -83,19 +136,16 @@ def main():
                 keyboard.add(key_ok, key_cancel)
                 bot.send_message(message.from_user.id, word, reply_markup=keyboard)
         else:
-            hat_game.add_word(message.text)
-            bot.send_message(message.from_user.id, 'Слово успешно добавлено')
-        print(hat_game.words)
-        print(hat_game.used_words)
+            bot.send_message(message.from_user.id, hat_game.add_word(message.text))
  
 
-    def get_teams_cnt(message): #количество команд
+    def get_members_cnt(message): #количество команд
         try:
-            hat_game.teams_cnt = int(message.text)
+            hat_game.member_cnt = int(message.text)
         except:
             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
-        bot.send_message(message.from_user.id, f'Играет {hat_game.teams_cnt} команд');  
         hat_game.start_new_game()
+        bot.send_message(message.from_user.id, f'Отлично! Игра номер {hat_game.game_number}, по {hat_game.member_cnt} участников в команде');  
 
         
     @bot.callback_query_handler(func=lambda call: True)
@@ -105,10 +155,8 @@ def main():
         if d == 'yes':
             hat_game.check_word(w,True, int(team_number))
         elif d == 'no':
-            hat_game.check_word(w,False)
+            hat_game.check_word(w,False, int(team_number))
         bot.send_message(chat_id, 'Next')
-        print(hat_game.words)
-        print(hat_game.used_words)
 
     
     bot.polling(none_stop=True, interval=0)
--- tests/test_hat_bot.py
+++ tests/test_hat_bot.py
@@ -12,13 +12,36 @@ from hat_bot.hat_bot import HatBot
 
 test_hat_bot = HatBot()
 
-def test_start_new_game():
-    test_hat_bot.start_new_game()
-    w = test_hat_bot.get_word()
-    assert(w, None)
+def test_start_new_game(n):
+    #присваивание номера игры	
+	#вопрос - сколько игроков в команде?
+    game_number = test_hat_bot.start_new_game(n)
+    assert(game_number, None)
+    
+def test_registration(n, gamer_name):
+    #регистрация участников:
+	#добавление игроков в общий список. Номер игры + список логинов    
+    test_hat_bot.gamer_registration(n, gamer_name)
+    #проверяем, есть ли указанный геймер нейм в списке логинов
+    u = test_hat_bot.members[n]
+    assert(gamer_name in u)
+    
+def test_new_round():
+    #начало игрового раунда:      
+	#если список команд не сформирован, то сформировать
+	#если это не первый раунд, то перенести список слов из использованных в общий 
+    test_hat_bot.start_new_round()
+    cnt = len(test_hat_bot.team_members) > 0
+    assert(cnt, True)
+
+#def test_get_team_number(gamer_name):    
+    #получить номер команды по имени пользователя
+
 
 def test_add_word():
     test_hat_bot.add_word('word')
+    l = ('word' in test_hat_bot.words)
+    assert(l, True)
     
 def test_get_word():
     test_hat_bot.start_new_game()
